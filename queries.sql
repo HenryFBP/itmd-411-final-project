75c4c7f2d7389dd334e82178e58e7cdebbc21ecf
HenryFBP <HenryFBP@gmail.com>
2017-11-29 20:58:04 -0600

    restructure SQL and include test queries for pretty end-user viewing

--- queries.sql
+++ queries.sql
@@ -66,27 +66,45 @@ CREATE TABLE IF NOT EXISTS user_types
 DESCRIBE user_types;
 
 
-INSERT INTO categories (category) VALUES ('other'), ('tech'), ('building');
-
-INSERT INTO severities (severity_level) VALUES ('only me affected'), ('room affected'), ('office affected'), ('building affected'), ('armageddon');
-
-INSERT INTO user_types (user_type_string) VALUES ('NORMAL_USER'), ('ADMINISTRATOR');
- 
-INSERT INTO users VALUES (1,'HenryFBP','iamapassword','hpost@hawk.iit.edu','Henry Post',2);
-INSERT INTO users VALUES (2,'bolo','alsopassword','jbolo@hawk.iit.edu','Dartor Bolo',1);
-INSERT INTO users VALUES (3,'peter','passwordhere','peter@hawk.iit.edu','Peter',1);
-INSERT INTO users VALUES (4,'mike','mikerocks12345','mike@hawk.iit.edu','Michael Bernauer',1);
-INSERT INTO users VALUES (5,'max','password','max@hawk.iit.edu','Max Oellean',1);
-INSERT INTO users VALUES (6,'admin','','admin@admin.admin','Admin admin',2);
+INSERT INTO
+	categories (category)
+VALUES
+('other'),
+('tech'),
+('building');
 
 INSERT INTO
-	tickets
-	(PID, shortDesc, longDesc, CID, SID, startDate)
+	severities (severity_level)
+VALUES
+('only me affected'), 
+('room affected'), 
+('office affected'), 
+('building affected'), 
+('armageddon');
+
+INSERT INTO 
+	user_types (user_type_string) 
+VALUES
+('NORMAL_USER'),
+('ADMINISTRATOR');
+ 
+INSERT INTO
+	users (username, password, email, name, user_type)
+VALUES
+('HenryFBP','iamapassword','hpost@hawk.iit.edu','Henry Post',2),
+('bolo','alsopassword','jbolo@hawk.iit.edu','Dartor Bolo',1),
+('peter','passwordhere','peter@hawk.iit.edu','Peter',1),
+('mike','mikerocks12345','mike@hawk.iit.edu','Michael Bernauer',1),
+('max','password','max@hawk.iit.edu','Max Oellean',1),
+('admin','','admin@admin.admin','Admin admin',2);
+
+INSERT INTO
+	tickets (PID, shortDesc, longDesc, CID, SID, startDate)
 VALUES
 	(2, 'henry is asleep', 		'henry has fallen asleep at his desk', 1, 1, DATE(NOW())),
 	(1, 'bolo staring at me', 	'bolo is staring at me. I think he thinks i\'m asleep.', 1, 1, DATE(NOW()));
 
-
+-- 1 row(s) affected, 1 warning(s): 1292 Incorrect date value: '2017-11-30 19:09:37' for column 'endDate' at row 1 Rows matched: 1  Changed: 1  Warnings: 1
 
 SELECT * FROM users;
 SELECT * FROM tickets;
@@ -94,7 +112,39 @@ SELECT * FROM tickets;
 UPDATE tickets
 	SET endDate = ADDDATE(NOW(), INTERVAL 1 DAY)
     WHERE TID = 1;
-
-SELECT TID, name, t.PID, shortDesc, longDesc, CID, startDate, endDate FROM tickets t
-INNER JOIN (SELECT name, PID from users) u
-ON t.PID = u.PID
+    
+    
+SELECT
+	TID,
+    PID,
+    name,
+    RES.CID,
+    category,
+    SID,
+	shortDesc,
+    longDesc,
+    startDate,
+    endDate
+FROM
+	(
+	SELECT
+		TID,
+		t.PID,
+		name,
+		CID,
+        SID,
+		shortDesc,
+		longDesc, 
+		startDate, 
+		endDate 
+	FROM
+		tickets t
+	INNER JOIN
+		(SELECT name, PID from users) u
+	ON
+		t.PID = u.PID
+	) AS RES
+INNER JOIN
+	(SELECT CID, category FROM categories) c
+ON
+	c.CID = RES.CID
