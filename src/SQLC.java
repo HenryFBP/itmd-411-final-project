7e396f3f688589faff372cc940cb9756a9d0c6cd
HenryFBP <HenryFBP@gmail.com>
2017-11-29 20:58:22 -0600

    add SQL string formatting constants

--- src/SQLC.java
+++ src/SQLC.java
@@ -4,23 +4,30 @@
  */
 public enum SQLC
 {
-	 TABLE_USERS				("users"),
-	 TABLE_TICKETS				("tickets"),
-	 TABLE_USER_TYPES			("user_types"),
-	 TABLE_CATEGORIES			("categories"),
-	 TABLE_SEVERITIES			("severities"),
-	 
-	 PASSWORD_COLUMN_NAME		("password"),
-	 USERTYPE_COLUMN_NAME		("user_type"),
-	 USER_ID_COLUMN_NAME		("PID"),
-	 TICKET_ID_COLUMN_NAME		("TID"),
-	 SHORT_DESC_COLUMN_NAME		("shortDesc"),
-	 LONG_DESC_COLUMN_NAME		("longDesc"),
-	 CATEGORY_COLUMN_NAME		("category"),
-	 CATEGORY_ID_COLUMN_NAME	("CID"),
-	 SEVERITY_COLUMN_NAME		("SID"),
-	 START_DATE_COLUMN_NAME		("startDate"),
-	 END_DATE_COLUMN_NAME		("endDate");
+	
+	SQL_STR_TO_DATE_FORMAT	("%Y-%m-%d %H:%i:%s"),
+	SQL_DATE_FORMAT			("yyyy-MM-dd hh:mm:ss"),
+	
+	TABLE_USERS					("users"),
+	TABLE_TICKETS				("tickets"),
+	TABLE_USER_TYPES			("user_types"),
+	TABLE_CATEGORIES			("categories"),
+	TABLE_SEVERITIES			("severities"),
+
+	PASSWORD_COLUMN_NAME		("password"),
+	USERTYPE_COLUMN_NAME		("user_type"),
+	USER_ID_COLUMN_NAME			("PID"),
+	TICKET_ID_COLUMN_NAME		("TID"),
+	SHORT_DESC_COLUMN_NAME		("shortDesc"),
+	LONG_DESC_COLUMN_NAME		("longDesc"),
+	CATEGORY_COLUMN_NAME		("category"),
+	CATEGORY_ID_COLUMN_NAME		("CID"),
+	SEVERITY_COLUMN_NAME		("SID"),
+	START_DATE_COLUMN_NAME		("startDate"),
+	END_DATE_COLUMN_NAME		("endDate"),
+	
+	SEARCH_PANE_DEFAULT_QUERY ("SELECT * FROM " + SQLC.TABLE_TICKETS);
+
 	
 	private String constants;
 
