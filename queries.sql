676953de68fdf42cbfd775ebb828b0a230588921
Henry Post <HenryFBP@gmail.com>
2017-11-29 18:16:25 -0600

    add categories and severity tables

--- queries.sql
+++ queries.sql
@@ -16,46 +16,77 @@ CREATE TABLE IF NOT EXISTS users
 );	
 DESCRIBE users;
 
+DROP TABLE IF EXISTS categories;
+CREATE TABLE IF NOT EXISTS categories
+(
+	CID			INTEGER			NOT NULL UNIQUE AUTO_INCREMENT,
+    category	VARCHAR(64)		NOT NULL,
+    
+    PRIMARY KEY (CID)
+);
+DESCRIBE categories;
+
+DROP TABLE IF EXISTS severities;
+CREATE TABLE IF NOT EXISTS severities
+(
+	SID				INTEGER			NOT NULL UNIQUE AUTO_INCREMENT,
+    severity_level	VARCHAR(64)		NOT NULL,
+    
+    PRIMARY KEY (SID)
+);
+DESCRIBE severities;
+
+
 DROP TABLE IF EXISTS tickets;
 CREATE TABLE IF NOT EXISTS tickets 
 (
 	TID			INTEGER 		NOT NULL UNIQUE AUTO_INCREMENT,
     PID	 		INTEGER 		NOT NULL,
+    CID			INTEGER 		NOT NULL,
     shortDesc	TEXT		 	NOT NULL,
-    category	VARCHAR(20) 	NOT NULL,
-    severity	INTEGER 		NOT NULL,
+    SID			INTEGER 		NOT NULL,
     longDesc	TEXT,
     startDate	DATE 			NOT NULL,
     endDate		DATE,
     
     PRIMARY KEY (TID),
-    FOREIGN KEY (PID) REFERENCES users(PID)
+    FOREIGN KEY (PID) REFERENCES users(PID),
+    FOREIGN KEY (CID) REFERENCES categories(CID)
 );
 DESCRIBE tickets;
 
 DROP TABLE IF EXISTS user_types;
 CREATE TABLE IF NOT EXISTS user_types
 (
-	user_type			INTEGER			NOT NULL UNIQUE,
+	user_type			INTEGER			NOT NULL UNIQUE AUTO_INCREMENT,
     user_type_string	VARCHAR(20)		NOT NULL,
     
     PRIMARY KEY (user_type)
 );
+DESCRIBE user_types;
 
 
+INSERT INTO categories (category) VALUES ('other'), ('tech'), ('building');
 
+INSERT INTO severities (severity_level) VALUES ('only me affected'), ('room affected'), ('office affected'), ('building affected'), ('armageddon');
 
-INSERT INTO user_types VALUES(1, 'NORMAL_USER');
-INSERT INTO user_types VALUES(2, 'ADMINISTRATOR');
+INSERT INTO user_types (user_type_string) VALUES ('NORMAL_USER'), ('ADMINISTRATOR');
  
-INSERT INTO users VALUES(1,'HenryFBP','iamapassword','hpost@hawk.iit.edu','Henry Post',2);
-INSERT INTO users VALUES(2,'bolo','alsopassword','jbolo@hawk.iit.edu','Dartor Bolo',1);
-INSERT INTO users VALUES(3,'peter','passwordhere','peter@hawk.iit.edu','Peter',1);
-INSERT INTO users VALUES(4,'mike','mikerocks12345','mike@hawk.iit.edu','Michael Bernauer',1);
-INSERT INTO users VALUES(5,'max','password','max@hawk.iit.edu','Max Oellean',1);
+INSERT INTO users VALUES (1,'HenryFBP','iamapassword','hpost@hawk.iit.edu','Henry Post',2);
+INSERT INTO users VALUES (2,'bolo','alsopassword','jbolo@hawk.iit.edu','Dartor Bolo',1);
+INSERT INTO users VALUES (3,'peter','passwordhere','peter@hawk.iit.edu','Peter',1);
+INSERT INTO users VALUES (4,'mike','mikerocks12345','mike@hawk.iit.edu','Michael Bernauer',1);
+INSERT INTO users VALUES (5,'max','password','max@hawk.iit.edu','Max Oellean',1);
+INSERT INTO users VALUES (6,'admin','','admin@admin.admin','Admin admin',2);
 
-INSERT INTO tickets VALUES(1, 2, 'henry is asleep', 'other', 1, 'henry has fallen asleep at his desk', DATE(NOW()), null);
-INSERT INTO tickets VALUES(2, 1, 'bolo staring at me', 'other', 1, 'bolo is staring at me. I think he thinks i\'m asleep.', DATE(NOW()), null);
+INSERT INTO
+	tickets
+	(PID, shortDesc, longDesc, CID, SID, startDate)
+VALUES
+	(2, 'henry is asleep', 		'henry has fallen asleep at his desk', 1, 1, DATE(NOW())),
+	(1, 'bolo staring at me', 	'bolo is staring at me. I think he thinks i\'m asleep.', 1, 1, DATE(NOW()));
+
+
 
 SELECT * FROM users;
 SELECT * FROM tickets;
@@ -64,6 +95,6 @@ UPDATE tickets
 	SET endDate = ADDDATE(NOW(), INTERVAL 1 DAY)
     WHERE TID = 1;
 
-SELECT TID, name, t.PID, shortDesc, longDesc, category, startDate, endDate FROM tickets t
+SELECT TID, name, t.PID, shortDesc, longDesc, CID, startDate, endDate FROM tickets t
 INNER JOIN (SELECT name, PID from users) u
 ON t.PID = u.PID
